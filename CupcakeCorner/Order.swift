e43a3bf138bf5909b7abe48291136f603690f570
leila <143362178+1e1104k4@users.noreply.github.com>
2024-01-25 12:51:52 -0600

    extension to String - checking for emptiness

--- CupcakeCorner/Order.swift
+++ CupcakeCorner/Order.swift
@@ -43,7 +43,7 @@ class Order: Codable {
     var zip = ""
     
     var hasValidAddress: Bool {
-        if name.isEmpty || streetAddress.isEmpty || city.isEmpty || zip.isEmpty {
+        if name.isReallyEmpty || streetAddress.isReallyEmpty || city.isReallyEmpty || zip.isReallyEmpty {
             return false
         }
         return true
