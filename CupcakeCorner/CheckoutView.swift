3001b5b64aa0d4100e133542b80dcf1eb95872e7
leila <143362178+1e1104k4@users.noreply.github.com>
2024-01-24 22:24:37 -0600

    alert containing details of order

--- CupcakeCorner/CheckoutView.swift
+++ CupcakeCorner/CheckoutView.swift
@@ -10,6 +10,9 @@ import SwiftUI
 struct CheckoutView: View {
     var order: Order
     
+    @State private var confirmationMessage = ""
+    @State private var showingConfirmation = false
+    
     var body: some View {
         ScrollView {
             VStack {
@@ -35,6 +38,11 @@ struct CheckoutView: View {
         .navigationTitle("Check out")
         .navigationBarTitleDisplayMode(.inline)
         .scrollBounceBehavior(.basedOnSize)
+        .alert("Thank You!", isPresented: $showingConfirmation) { 
+            Button("OK") { }
+        } message: {
+            Text(confirmationMessage)
+        }
     }
     
     func placeOrder() async {
